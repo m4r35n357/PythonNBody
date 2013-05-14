1c6f112065bc297c0b99f7050a23b704345179bc
Ian Smith <m4r35n357@gmail.com>
2013-05-14 20:20:59 +0100

    read JSON from named file as parameter

--- filegraphics.py
+++ filegraphics.py
@@ -17,15 +17,17 @@ scene.height = 800
 scene.range = (10.0, 10.0, 10.0)
 colours = [ (1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0), (0.5, 0.5, 0.0), (0.5, 0.0, 0.5), (0.0, 0.5, 0.5), (0.7, 0.7, 0.7), (1.0, 1.0, 1.0) ]
 spheres = []
-data = readJson('./aaa')
-for i in range(len(data[0])):
+data = readJson(sys.argv[1])
+rowRange = range(len(data))
+particleRange = range(len(data[0]))
+for i in particleRange:
 	p = data[0][i]
 	ball = sphere(pos = (p['qX'], p['qY'], p['qZ']), radius = 0.05 * math.pow(p['mass'], 1.0 / 3.0), color = colours[i])
 	ball.trail = curve(color = ball.color)
 	spheres.append(ball)
-for i in range(len(data)):
+for i in rowRange:
 	rate(60)
-	for j in range(len(data[0])):
+	for j in particleRange:
 		p = data[i][j]
 		ball = spheres[j]
 		position = (p['qX'], p['qY'], p['qZ'])
