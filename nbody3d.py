b0b719713cd0665818d829b42407ba021a350044
Ian Smith <m4r35n357@gmail.com>
2013-05-24 19:48:52 +0100

    return of the 1st and 10th order

--- nbody3d.py
+++ nbody3d.py
@@ -27,7 +27,9 @@ class Symplectic(object):
 		self.ts = timeStep
 		self.eMax = errorLimit
 		self.n = simulationTime / math.fabs(timeStep)  # we can run backwards too!
-		if (order == 2):  # Second order
+		if (order == 1):  # First order
+			self.iterate = self.euler
+		elif (order == 2):  # Second order
 			self.iterate = self.stormerVerlet2
 		elif (order == 4):  # Fourth order
 			self.iterate = self.stormerVerlet4
@@ -35,8 +37,10 @@ class Symplectic(object):
 			self.iterate = self.stormerVerlet6
 		elif (order == 8):  # Eighth order
 			self.iterate = self.stormerVerlet8
+		elif (order == 10):  # Tenth order
+			self.iterate = self.stormerVerlet10
 		else:  # Wrong value for integrator order
-			raise Exception('>>> ERROR! Integrator order must be 2, 4, 6 or 8 <<<')
+			raise Exception('>>> ERROR! Integrator order must be 1, 2, 4, 6, 8 or 10 <<<')
 
 	def modR (self, xA, yA, zA, xB, yB, zB):  # Euclidean distance between point A and point B
 		return math.sqrt(math.pow(xB - xA, 2) + math.pow(yB - yA, 2) + math.pow(zB - zA, 2))
@@ -77,7 +81,11 @@ class Symplectic(object):
 					b.pY += dPy
 					b.pZ += dPz
 
-	def sympBase (self, c):
+	def euler (self):  # First order
+		self.updateQ(1.0)
+		self.updateP(1.0)
+
+	def sympBase (self, c):  # build higher order integrators by composition
 		self.updateQ(0.5 * c)
 		self.updateP(c)
 		self.updateQ(0.5 * c)
@@ -116,6 +124,41 @@ class Symplectic(object):
 		self.sympBase(0.19075471029623837995387626)
 		self.sympBase(-0.40910082580003159399730010)
 		self.sympBase(0.74167036435061295344822780)
+
+	def stormerVerlet10 (self):  # Tenth order
+		self.sympBase(0.09040619368607278492161150)
+		self.sympBase(0.53591815953030120213784983)
+		self.sympBase(0.35123257547493978187517736)
+		self.sympBase(-0.31116802097815835426086544)
+		self.sympBase(-0.52556314194263510431065549)
+		self.sympBase(0.14447909410225247647345695)
+		self.sympBase(0.02983588609748235818064083)
+		self.sympBase(0.17786179923739805133592238)
+		self.sympBase(0.09826906939341637652532377)
+		self.sympBase(0.46179986210411860873242126)
+		self.sympBase(-0.33377845599881851314531820)
+		self.sympBase(0.07095684836524793621031152)
+		self.sympBase(0.23666960070126868771909819)
+		self.sympBase(-0.49725977950660985445028388)
+		self.sympBase(-0.30399616617237257346546356)
+		self.sympBase(0.05246957188100069574521612)
+		self.sympBase(0.44373380805019087955111365)
+		self.sympBase(0.05246957188100069574521612)
+		self.sympBase(-0.30399616617237257346546356)
+		self.sympBase(-0.49725977950660985445028388)
+		self.sympBase(0.23666960070126868771909819)
+		self.sympBase(0.07095684836524793621031152)
+		self.sympBase(-0.33377845599881851314531820)
+		self.sympBase(0.46179986210411860873242126)
+		self.sympBase(0.09826906939341637652532377)
+		self.sympBase(0.17786179923739805133592238)
+		self.sympBase(0.02983588609748235818064083)
+		self.sympBase(0.14447909410225247647345695)
+		self.sympBase(-0.52556314194263510431065549)
+		self.sympBase(-0.31116802097815835426086544)
+		self.sympBase(0.35123257547493978187517736)
+		self.sympBase(0.53591815953030120213784983)
+		self.sympBase(0.09040619368607278492161150)
 
 	def bodiesJson (self):
 		data = []
